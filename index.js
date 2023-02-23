2a75877ba62647e5a1e12e47c6d2f6069d773c7c
Marc Norris <marcnorris007@icloud.com>
2023-02-23 21:54:03 +0000

    created employee class

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,4 +13,21 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+let team = [];
+
+startProgram()
+async function startProgram() {
+
+
+    team.push(new Employee("Andrew", 1, "test@test.com"))
+
+    //  Use FS.write to write the following to file
+    let htmlDoc = render(team);
+
+    await fs.writeFile(outputPath, htmlDoc);
+
+}
+
+
 
