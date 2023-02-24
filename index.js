7402fcffbfcc4da90ef8cde97622e1e7eb9b3655
Marc Norris <marcnorris007@icloud.com>
2023-02-24 21:34:17 +0000

    added inquirer questions

--- index.js
+++ index.js
@@ -13,50 +13,126 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-class Questions {
-  constructor() {
-    this.teamArr = [];
-  }
 
-  getTeamArr() {
-    return this.teamArr;
-  }
+let team = [];
 
   // Questions go here!
 
-  questions() {
-    inquirer.prompt({
-      type: "",
-      name: "",
-      message: "",
-      choices: [],
-    });
+  async function questions() {
+    let data = await inquirer.prompt(
+        [{
+      type: "list",
+      name: "role",
+      message: "Select the type of employee you would like to add",
+      choices: ["Manager", "Engineer", "Intern"]
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your id number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please enter your office number',
+        when(answers) {
+            return answers.role === 'Manager';
+        }
+        // validate(value) {
+        //     const pass = value.match(
+        //         /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i 
+        //     );
+        //     if (pass) {
+        //         return true;
+        //     }
+        //     return 'Please enter a valid number';
+
+        // }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter school'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'please provide your github username'
+    }
+
+]
+    )
+    
+        const manager1 = new Manager(data.name, data.id , data.email , data.officeNumber );
+        team.push(manager1);
+
+    
+        // const engineer1 = new Engineer(data.name, data.id, data.email, data.github);
+        // team.push(engineer1);
+    
+        // const intern1 = new Intern("Jessica", 009, "jessica@test.com", "MIT");
+        // team.push(intern1);
+
+    let htmlDoc = render(team);
+    
+    await fs.writeFile(outputPath, htmlDoc);
+
   }
-}
+
+  questions();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // The data below was for testing purposes
 
-let team = [];
 
-startProgram();
-async function startProgram() {
-  // team.push(new Employee("Andrew", 1, "test@test.com"))
 
-  const manager1 = new Manager("Marc", 007, "marc@test.com", 0745674833);
-  team.push(manager1);
+// startProgram();
+// async function startProgram() {
+//   // team.push(new Employee("Andrew", 1, "test@test.com"))
 
-  const engineer1 = new Engineer("Arnold", 008, "david@test.com", "David_008");
-  team.push(engineer1);
+//   const manager1 = new Manager("Lee", 007, "marc@test.com", 0745674833);
+//   team.push(manager1);
 
-  const intern1 = new Intern("Jessica", 009, "jessica@test.com", "MIT");
-  team.push(intern1);
+//   const engineer1 = new Engineer("Arnold", 008, "david@test.com", "David_008");
+//   team.push(engineer1);
 
-  // const intern1 = new Intern()
-  // team.push(manager1);
+//   const intern1 = new Intern("Jessica", 009, "jessica@test.com", "MIT");
+//   team.push(intern1);
 
-  //  Use FS.write to write the following to file
-  let htmlDoc = render(team);
+ 
 
-  await fs.writeFile(outputPath, htmlDoc);
-}
+
+
+//   let htmlDoc = render(team);
+
+//   await fs.writeFile(outputPath, htmlDoc);
+
--- src/page-template.js
+++ src/page-template.js
@@ -45,7 +45,7 @@ const generateTeam = team => {
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <h3 class="card-title"><i class="fa-solid fa-graduation-cap"></i>${intern.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
