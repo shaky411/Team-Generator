ef2ec4d19b6225322d6c180798525f34e9ca2718
Marc Norris <marcnorris007@icloud.com>
2023-02-27 21:37:51 +0000

    redone index.js from the start

--- index.js
+++ index.js
@@ -1,151 +1,157 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs/promises");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-
+// Empty array to store results that will be printed to HTML doc
 let team = [];
 
-  // Questions go here!
-
-  async function questions() {
+// Start with manger details then initiate questions for employees
+async function startProgram() {
     let data = await inquirer.prompt(
-        [{
-      type: "list",
-      name: "role",
-      message: "Select the type of employee you would like to add",
-      choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your id number?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email address'
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Please enter your office number',
-        when(answers) {
-            return answers.role === 'Manager';
-        },
-        validate(value) {
-            const pass = value.match(
-                /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i 
-            );
-            if (pass) {
-                return true;
+        [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+        
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id number?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your email address'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter your office number'
             }
-            return 'Please enter a valid number';
-
-        }
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'Please enter school',
-        when(answers) {
-            return answers.role === "Intern";
-        }
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'please provide your github username',
-        when(answers) {
-            return answers.role === "Engineer"
-        }
-    }
-
-]
+        ]
     )
 
-    
-    if (data.role === "Manager") {
-        const manager1 = new Manager(data.name, data.id , data.email , data.officeNumber );
-        team.push(manager1);
+    // pushes data to the team array
+    const managerAnswers = new Manager(data.name, data.id, data.email, data.officeNumber)
+    team.push(managerAnswers)
+
+    // Starts employee questions
+    return employeeQuestions();
+}
+
+// This starts the programme and initiates the manager questions
+startProgram()
+
+// Questions about the employees
+async function employeeQuestions() {
+    let employeeData = await inquirer.prompt(
+        [
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Select the type of employee you would like to add',
+            choices: ["Engineer", "Intern", "No more entries to add"]
+        }
+    ])
+
+    // Show questions based on role selection
+    if (employeeData.role === "Engineer") {
+        return engineerQuestions();
     }
 
-    if (data.role === "Engineer") {
-        const engineer1 = new Engineer(data.name, data.id, data.email, data.github);
-        team.push(engineer1);
+    if (employeeData.role === "Intern") {
+        return internQuestions();
     }
 
-    if (data.role === "Intern") {
-        const intern1 = new Intern(data.name, data.id, data.email, data.school);
-        team.push(intern1);
+    // If no more entries selected, create the HTML file
+    if (employeeData.role === "No more entries to add") {
+        return writeToFile();
     }
+}
 
+
+
+async function engineerQuestions() {
+    let engineerData = await inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+        
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id number?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your email address'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'please provide your github username'
+            }
+        ]
+    )
+
+    const engineerAnswers = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
+    team.push(engineerAnswers)
+
+    return employeeQuestions()
+
+}
+
+async function internQuestions() {
+    let internData = await inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+        
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id number?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your email address'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'please enter school name'
+            }
+        ]
+    )
+
+    const internAnswers = new Intern(internData.name, internData.id, internData.email, internData.school)
+    team.push(internAnswers)
+
+    return employeeQuestions()
+
+}
+
+
+// Function to write the data to the html file
+async function writeToFile() {
     let htmlDoc = render(team);
     await fs.writeFile(outputPath, htmlDoc);
-
-  }
-
-  
-
-  questions();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// The data below was for testing purposes
-
-
-
-// startProgram();
-// async function startProgram() {
-//   // team.push(new Employee("Andrew", 1, "test@test.com"))
-
-//   const manager1 = new Manager("Lee", 007, "marc@test.com", 0745674833);
-//   team.push(manager1);
-
-//   const engineer1 = new Engineer("Arnold", 008, "david@test.com", "David_008");
-//   team.push(engineer1);
-
-//   const intern1 = new Intern("Jessica", 009, "jessica@test.com", "MIT");
-//   team.push(intern1);
-
- 
-
-
-
-//   let htmlDoc = render(team);
-
-//   await fs.writeFile(outputPath, htmlDoc);
-
+}
